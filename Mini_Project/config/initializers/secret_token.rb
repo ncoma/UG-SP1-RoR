b31db7764278bf072b8b0a9e6030865b6858f042
Pablo <jpms9308@gmail.com>
2014-04-02 22:14:10 -0600

    lab2

---
+++ Mini_Project/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniProject::Application.config.secret_key_base = 'f546eb5ada645d47f90b1a0dd8cef3f271a9a7b62d69b06a9980a5e63952364280de70e3e3add239bb307fd30f27312428fc6a172eaadeef3fed4864eb027eb2'
